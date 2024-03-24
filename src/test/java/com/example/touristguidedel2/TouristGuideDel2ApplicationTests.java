34f45d60b97e1a3992a8d7e147b3d111f158da94
Almer Toro Coskun <alco0001@stud.kea.dk>
2024-03-24 16:29:26 +0100

    Programmet kører nu igen

--- src/test/java/com/example/touristguidedel2/TouristGuideDel2ApplicationTests.java
+++ src/test/java/com/example/touristguidedel2/TouristGuideDel2ApplicationTests.java
@@ -1,11 +1,21 @@
 package com.example.touristguidedel2;
 
+import com.example.touristguidedel2.Controller.TouristController;
+import com.example.touristguidedel2.model.TouristAttraction;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TouristGuideDel2ApplicationTests {
 
+   /* @Autowired
+    private TouristController touristController;
+
+    @Autowired
+    private
+
+*/
     @Test
     void contextLoads() {
     }
